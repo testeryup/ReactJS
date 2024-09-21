cbee6bce0d3aac83b8ecfbb8f98d5df30e0f1dc5
testeryup <enzo.edwin.zzz@gmail.com>
2024-09-21 22:33:47 +0700

    git set state, add scss, table row, function map, adminMenu learn about react life cycle include: construct, did mound, render

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,80 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { getAllUsers } from '../../services/userService';
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props){
+        super(props);
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if(response && response.errCode === 0){
+            this.setState({
+                arrUsers: response.users
+            }, () => {
+                console.log(this.state.arrUsers)
+            })
+        }
+        console.log('get all users: ', response);
     }
-
+    /** Life cycle
+     * Run component
+     * 1. Run construct -> init state
+     * 2. Did mount
+     * 3. Render
+     * 
+     * 
+     * 
+     */
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-center'>Manage User</div>
+                <div className='users-table mt-3 mx-1'>
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        
+                        { arrUsers && arrUsers.map((item, index) => {
+                            console.log('checkmap', item, index)
+                            return(
+                                <tr>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit'>
+                                            <i class="fas fa-pencil-alt"></i>
+                                        </button>
+                                        <button className='btn-delete'>
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                            
+                        
+                        
+                    </table>
+                </div>
+                    
+                
+            </div>
         );
     }
 
