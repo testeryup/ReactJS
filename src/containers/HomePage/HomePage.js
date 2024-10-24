8add61a0964f12168d35823e4b6c84a47bc5f676
testeryup <enzo.edwin.zzz@gmail.com>
2024-10-24 11:13:47 +0700

    css sections

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from "./HomeHeader";
 import Specialty from './Section/Specialty';
+import MedicalFacility from './Section/MedicalFacility';
+import OustandingDoctor from './Section/OustandingDoctor';
+import HandBook from './Section/HandBook';
+import './HomePage.scss';
 
-
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 class HomePage extends Component {
 
     render() {
-
+        const settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+        };
         return (
             <div>
                 <HomeHeader>
 
                 </HomeHeader>
-                <Specialty></Specialty>
+                <Specialty
+                    settings={settings}
+                ></Specialty>
+                <MedicalFacility
+                    settings={settings}
+                ></MedicalFacility>
+                <OustandingDoctor
+                    settings={settings}
+                >
+                </OustandingDoctor>
+                <HandBook
+                    settings={settings}
+                ></HandBook>
             </div>
         );
     }
@@ -21,6 +45,7 @@ class HomePage extends Component {
 }
 
 const mapStateToProps = state => {
+    
     return {
         isLoggedIn: state.user.isLoggedIn
     };
